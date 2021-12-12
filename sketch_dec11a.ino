f14afd94ec1f0181ec894240fe2cdd7a5aee3cc9
tomasz <tomaszkacperkulig@gmail.com>
2021-12-12 15:28:18 +0100

    Btc working

--- sketch_dec11a.ino
+++ sketch_dec11a.ino
@@ -2,6 +2,8 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define MAX_DEVICES 4 //number of led matrix connect in series
 #define CS_PIN 15
@@ -16,13 +18,25 @@ const char* password = "***REMOVED***";
 // SOFTWARE SPI
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
-uint8_t scrollSpeed = 42;    // default frame delay value
+uint8_t scrollSpeed = 48;    // default frame delay value
 textEffect_t scrollEffect = PA_SCROLL_LEFT;
 textPosition_t scrollAlign = PA_LEFT;
 uint16_t scrollPause = 2000; // in milliseconds
 
+HTTPClient http;
+const int httpsPort = 443;                                                          //Bitcoin price API powered by CoinDesk - https://www.coindesk.com/price/bitcoin
+const String url = "http://api.coindesk.com/v1/bpi/currentprice/BTC.json";
+const String historyURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
+const String cryptoCode = "BTC";
+
+struct Data {
+  String price;
+  String yesterdayChange;
+};
+
 void setup()
 {
+  Serial.begin(115200);
   P.begin();
   connectingWifi();
 }
@@ -30,12 +44,95 @@ void setup()
 void connectingWifi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    display("Connecting...", 1000);
+    displayMsg("Connecting...", 1000);
   }
-  display("Connected!", 3000);
+  displayMsg("Connected!", 5000);
 }
 
-void display(char* text, long milis) {
+void loop()
+{
+  Data d = getData();
+  String text = d.price + " BTC/USD " + "   " + d.yesterdayChange + "%";
+  const char* msg = {text.c_str()};
+  displayMsg(msg, 60000);
+}
+
+Data getData() {
+  Serial.print("Connecting to ");                                                       //Display url on Serial monitor for debugging
+  Serial.println(url);
+
+  http.begin(url);
+  int httpCode = http.GET();                                                            //Get crypto price from API
+  StaticJsonDocument<2000> doc;
+  DeserializationError error = deserializeJson(doc, http.getString());
+
+  if (error)                                                                            //Display error message if unsuccessful
+  {
+    Serial.print(F("deserializeJson Failed"));
+    Serial.println(error.f_str());
+    delay(2500);
+    return (Data) {
+      "", ""
+    };
+  }
+
+  Serial.print("HTTP Status Code: ");
+  Serial.println(httpCode);
+
+  String BTCUSDPrice = doc["bpi"]["USD"]["rate_float"].as<String>();                    //Store crypto price and update date in local variables
+  String lastUpdated = doc["time"]["updated"].as<String>();
+  http.end();
+
+  Serial.print("Getting history...");
+  StaticJsonDocument<2000> historyDoc;
+  http.begin(historyURL);                                                               //Get historical crypto price from API
+  int historyHttpCode = http.GET();
+  DeserializationError historyError = deserializeJson(historyDoc, http.getString());
+
+  if (historyError) {                                                                   //Display error message if unsuccessful
+    Serial.print(F("deserializeJson(History) failed"));
+    Serial.println(historyError.f_str());
+    delay(2500);
+    return (Data) {
+      "", ""
+    };
+  }
+
+  Serial.print("History HTTP Status Code: ");
+  Serial.println(historyHttpCode);
+  JsonObject bpi = historyDoc["bpi"].as<JsonObject>();
+  double yesterdayPrice;
+  for (JsonPair kv : bpi) {
+    yesterdayPrice = kv.value().as<double>();                                           //Store yesterday's crypto price
+  }
+
+  Serial.print("BTCUSD Price: ");                                                       //Display current price on serial monitor
+  Serial.println(BTCUSDPrice.toDouble());
+
+  Serial.print("Yesterday's Price: ");                                                  //Display yesterday's price on serial monitor
+  Serial.println(yesterdayPrice);
+
+  bool isUp = BTCUSDPrice.toDouble() > yesterdayPrice;                                  //Check whether price has increased or decreased
+  double percentChange;
+  String dayChangeString = "24hr. Change: ";
+  if (isUp)                                                                             //If price has increased from yesterday
+  {
+    percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
+  }
+  else                                                                                  //If price has decreased from yesterday
+  {
+    percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
+    dayChangeString = dayChangeString + "-";
+  }
+
+  Serial.print("Percent Change: ");                                                     //Display the percentage change on the serial monitor
+  Serial.println(percentChange);
+  return (Data) {
+    BTCUSDPrice, String(percentChange, 1)
+  };
+}
+
+void displayMsg(const char* text, long milis) {
   long startTime = millis();
   long elapsedTime = 0;
   while (elapsedTime < milis) {
@@ -43,11 +140,6 @@ void display(char* text, long milis) {
     {
       P.displayText(text, scrollAlign, scrollSpeed, 0, scrollEffect, scrollEffect);
     }
-    elapsedTime = millis() - milis;
+    elapsedTime = millis() - startTime;
   }
-}
-
-void loop()
-{
-  display("Hello! Enter new message?", 1);
 }
