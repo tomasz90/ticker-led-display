02263cdefd67ca21eedffb20a6224e0756e5f410
tomasz <tomaszkacperkulig@gmail.com>
2021-12-12 23:35:21 +0100

    getting data working

--- sketch_dec11a.ino
+++ sketch_dec11a.ino
@@ -14,8 +14,8 @@
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 
-const char* ssid = "***REMOVED***";
-const char* password = "***REMOVED***";
+char* ssid = "***REMOVED***";
+char* password = "***REMOVED***";
 
 unsigned long interval = 86400000;
 unsigned long nextUpdateTime = 0;
@@ -23,11 +23,6 @@ String yesterday = "";
 
 // SOFTWARE SPI
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
-
-uint8_t scrollSpeed = 48;    // default frame delay value
-textEffect_t scrollEffect = PA_SCROLL_LEFT;
-textPosition_t scrollAlign = PA_LEFT;
-uint16_t scrollPause = 2000; // in milliseconds
 
 const int httpsPort = 443;                                                          //Bitcoin price API powered by CoinDesk - https://www.coindesk.com/price/bitcoin
 const String url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=USD";
@@ -65,20 +60,15 @@ void loop()
 String getCurrentDate() {
   unsigned long currentTime = millis();
   if (nextUpdateTime - currentTime < 0 || nextUpdateTime - currentTime > interval) {
-    Serial.print("updating time..");
     time_t nowS = DateTime.now();
     long int sec = 1639329382;
     Serial.print("Now seconds: ");
     Serial.print(sec);
     unsigned long timeTillUpdateMs = (interval - (sec % interval / 1000)) * 1000;
     nextUpdateTime = millis() + timeTillUpdateMs;
- 
     unsigned long t = sec - interval / 1000;
-    Serial.print(" Yesterday seconds: ");
-    Serial.print(t);
-
     Serial.print(" Yesterday was: ");
-    char buffer[40];
+    char buffer[80];
     sprintf(buffer, "%02d-%02d-%04d", day(t), month(t), year(t));
     yesterday = String(buffer);
   }
@@ -86,14 +76,14 @@ String getCurrentDate() {
 }
 
 Data getData(String date) {
-  
+
   HTTPClient http;
   Serial.print("Connecting to ");                                                       //Display url on Serial monitor for debugging
   Serial.println(url);
 
   http.begin(url);
   int httpCode = http.GET();                                                            //Get crypto price from API
-  StaticJsonDocument<2000> doc;
+  StaticJsonDocument<256> doc;
   DeserializationError error = deserializeJson(doc, http.getString());
 
   if (error)                                                                            //Display error message if unsuccessful
@@ -109,17 +99,17 @@ Data getData(String date) {
   Serial.print("HTTP Status Code: ");
   Serial.println(httpCode);
 
-  String BTCUSDPrice = doc["ethereum"]["usd"].as<String>();                    //Store crypto price and update date in local variables
+  String BTCUSDPrice = doc["ethereum"]["usd"].as<String>();
   http.end();
-  
+
   Serial.print("ETH price: " + BTCUSDPrice);
   Serial.print("Getting history...");
-  StaticJsonDocument<2000> historyDoc;
-  http.begin(historyURL+date);                                                               //Get historical crypto price from API
+  DynamicJsonDocument historyDoc(16000);
+  http.begin(historyURL + date);
   int historyHttpCode = http.GET();
   DeserializationError historyError = deserializeJson(historyDoc, http.getString());
 
-  if (historyError) {                                                                   //Display error message if unsuccessful
+  if (historyError) {
     Serial.print(F("deserializeJson(History) failed"));
     Serial.println(historyError.f_str());
     delay(2500);
@@ -128,35 +118,18 @@ Data getData(String date) {
     };
   }
 
-  Serial.print("History HTTP Status Code: ");
-  Serial.println(historyHttpCode);
-  JsonObject bpi = historyDoc["bpi"].as<JsonObject>();
-  double yesterdayPrice;
-  for (JsonPair kv : bpi) {
-    yesterdayPrice = kv.value().as<double>();                                           //Store yesterday's crypto price
-  }
+  String yesterdayPrice = historyDoc["market_data"]["current_price"]["usd"].as<String>();
+  http.end();
 
-  Serial.print("BTCUSD Price: ");                                                       //Display current price on serial monitor
+  Serial.print("BTCUSD Price: ");
   Serial.println(BTCUSDPrice.toDouble());
 
-  Serial.print("Yesterday's Price: ");                                                  //Display yesterday's price on serial monitor
+  Serial.print("Yesterday's Price: ");
   Serial.println(yesterdayPrice);
 
-  bool isUp = BTCUSDPrice.toDouble() > yesterdayPrice;                                  //Check whether price has increased or decreased
-  double percentChange;
-  String dayChangeString = "24hr. Change: ";
-  if (isUp)                                                                             //If price has increased from yesterday
-  {
-    percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
-  }
-  else                                                                                  //If price has decreased from yesterday
-  {
-    percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
-    dayChangeString = dayChangeString + "-";
-  }
+  double yPrice = yesterdayPrice.toDouble();
+  double percentChange = (BTCUSDPrice.toDouble() - yPrice / yPrice) * 100;
 
-  Serial.print("Percent Change: ");                                                     //Display the percentage change on the serial monitor
-  Serial.println(percentChange);
   return (Data) {
     BTCUSDPrice, String(percentChange, 1)
   };
@@ -168,7 +141,7 @@ void displayMsg(const char* text, long milis) {
   while (elapsedTime < milis) {
     if (P.displayAnimate())
     {
-      P.displayText(text, scrollAlign, scrollSpeed, 0, scrollEffect, scrollEffect);
+      P.displayText(text, PA_LEFT, 48, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
     }
     elapsedTime = millis() - startTime;
   }
